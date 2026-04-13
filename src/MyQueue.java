bfe2ce52cad5abc3710781223eda8f7b9430b5ca
Мирас <adilbayev.miras.edu@gmail.com>
2026-04-13 12:26:06 +0500

    Add MyQueue implementation using MyArrayList (FIFO structure)

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,2 +1,31 @@
-public class MyQueue {
+public class MyQueue<T> {
+    private final MyArrayList<T> list = new MyArrayList<>();
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean empty() {
+        return list.size() == 0;
+    }
+
+    public void enqueue(T item) {
+        list.add(item); // в конец
+    }
+
+    public T dequeue() {
+        if (empty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        T value = list.get(0);
+        list.remove(0);
+        return value;
+    }
+
+    public T peek() {
+        if (empty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return list.get(0);
+    }
 }
